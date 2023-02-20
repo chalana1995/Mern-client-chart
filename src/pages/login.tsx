5d8aa05d6096e7277824df09d2979c2329237617
chalana kalpitha <chalanakalpitha72@gmail.com>
2023-02-20 22:03:18 +0530

    side bar half done

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -40,13 +40,7 @@ export const Login: React.FC = () => {
   };
 
   return (
-    <Box
-      component="div"
-      sx={{
-        background: `radial-gradient(50% 50% at 50% 50%, #63386A 0%, #310438 100%)`,
-        backgroundSize: "cover",
-      }}
-    >
+    <Box component="div" sx={{ backgroundColor: "#FCFCFC" }}>
       <Container
         component="main"
         maxWidth="xs"
@@ -66,7 +60,7 @@ export const Login: React.FC = () => {
           }}
         >
           <div>
-            <img src="./refine.svg" alt="Refine Logo" />
+            <img src={yariga} alt="Refine Logo" />
           </div>
           <Box mt={4}>
             <GoogleButton />
